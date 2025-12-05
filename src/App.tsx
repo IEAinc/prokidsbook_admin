a92f78deac3e9842d1754fb4d15b0dd25889895c
ty.kim <ty.kim@iea.co.kr>
2025-12-05 09:49:13 +0900

    fix/daxhboard > detail page 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import Layout from './components/layouts/Layout'
 import Login from './pages/auth/Login.tsx'
 //대시보드
 import Dashboard from './pages/dashboard/Dashboard.tsx' //대시보드
-import VisitorCount from './pages/dashboard/VisitorCount.tsx' //방문자 수
-import DownloadCount from './pages/dashboard/DownloadCount.tsx' //다운로드 수
-import UserStatus from './pages/dashboard/UserStatus.tsx' //가입자 현황
-import StoryStatus from './pages/dashboard/StoryStatus.tsx' //동화 생성 현황
-import CharacterStatus from './pages/dashboard/CharacterStatus.tsx'
+import DetailPage from './components/pages/dashBoard/DetailPage.tsx' //대시보드 상세
 //사용자 관리
 import UserManagement from './pages/users/UserManagement.tsx' //회원 관리
 import UserDetail from './pages/users/UserDetail.tsx' //회원 관리 상세
@@ -40,11 +36,11 @@ function App() {
       <Route path="*" element={<NotFound />} />
       <Route element={<Layout />}>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/visitor" element={<VisitorCount />} />
-        <Route path="/dashboard/download" element={<DownloadCount />} />
-        <Route path="/dashboard/user" element={<UserStatus />} />
-        <Route path="/dashboard/story" element={<StoryStatus />} />
-        <Route path="/dashboard/character" element={<CharacterStatus />} />
+        <Route path="/dashboard/visitor" element={<DetailPage type="visitor" title="방문자 수" />} />
+        <Route path="/dashboard/download" element={<DetailPage type="download" title="다운로드 수" />} />
+        <Route path="/dashboard/user" element={<DetailPage type="account" title="가입자 현황" />} />
+        <Route path="/dashboard/story" element={<DetailPage type="stories" title="동화 생성 현황" />} />
+        <Route path="/dashboard/character" element={<DetailPage type="characters" title="캐릭터 생성 현황" />} />
         <Route path="/users/management" element={<UserManagement />} />
         <Route path="/users/detail/:id" element={<UserDetail />} />
         <Route path="/users/images/:id" element={<ImageDetail />} />
